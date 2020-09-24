bd917c2330af49152ecbdfe0a680f85aa2dc00fd
shaikha-305 <shaikhaaljenaidel@gmail.com>
2020-09-24 16:40:43 +0300

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by shaikha aljenaidel on 9/24/20.
+//  Copyright © 2020 shaikha aljenaidel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
